4cdfa897e2a32007b1bed9f6c4f63bf24d26b917
usager <usager@10.0.2.15>
2019-11-22 18:37:56 -0500

    //version1.1

---
+++ src/test/java/com/mycompany/projetmaven2/OperationTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projetmaven2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author usager
+ */
+public class OperationTest {
+    
+    public OperationTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+       int a =0;
+       int b=0;
+       int res = Operation.add(a, b);
+        assertEquals(res, a+b);
+    }
+    
+}
